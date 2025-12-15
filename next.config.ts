ee963b475f7e056b3b04998db9d15429b2f3dc2a
Silas Postma <silas.postma@student.uva.nl>
2025-12-15 15:27:25 +0100

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: isGithubPages ? "/Watsof" : "",
+  assetPrefix: isGithubPages ? "/Watsof/" : "",
 };
 
 export default nextConfig;
